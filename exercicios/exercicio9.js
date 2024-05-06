ddd9ca6c448bc4ae6ad37c90ec39143fa582bdc5
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-05-06 10:59:27 -0300

    feat: exercicio9

---
+++ exercicios/exercicio9.js
@@ -0,0 +1,41 @@
+const prompt = require('prompt-sync')();
+
+let flag = true;
+function sumGenderSalary(){
+  let maleSalary = 0;
+  let femaleSalary = 0;
+
+  while(flag){
+    let salary = parseFloat(prompt('Qual o seu salário? ' ));
+    if(isNaN(salary)){
+      flag = false;
+      return `Valor invalido`
+    }
+    let gender = prompt('Qual o seu gênero ? M-masculino F-feminino? ');
+    if(gender.toLowerCase() === 'm'){
+      maleSalary+= salary;
+    }else if(gender.toLowerCase() === 'f'){
+      femaleSalary += salary;
+    }else{
+      flag = false;
+      return `Valor invalido`
+    }
+
+    continueOrNot(flag);
+
+  }
+
+  return `Salario pago aos homens = ${maleSalary} \nSalario pago as mulheres = ${femaleSalary}`
+
+}
+
+
+function continueOrNot(){
+  let decision = prompt('Deseja continuar ? s-sim n-não? ');
+
+  if(decision.toLowerCase() === 'n'){
+    return flag = false;
+  }
+}
+
+console.log(sumGenderSalary());
