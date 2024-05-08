50dab96b2853a36496c92eb56c6f4dfbc834ae95
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-05-08 06:55:14 -0300

    feat: exercicio18

---
+++ exercicios/exercicio18.js
@@ -0,0 +1,23 @@
+const prompt = require('prompt-sync')();
+
+
+
+function showEmployee(){
+ let employee = {
+    nome : '',
+    cargo : '',
+    salario : 0.0
+  }
+  getEmplyeeInfos(employee);
+
+  return employee;
+}
+
+function getEmplyeeInfos(employee){
+  employee.nome = prompt("Digite o nome: ");
+  employee.cargo = prompt("Digite o cargo: ");
+  employee.salario = parseFloat(prompt("Digite o salário: "));
+}
+
+
+console.log(showEmployee());
