e03429d7a95e8c1fbab935ff94fb6dcf0e2313c8
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-05-09 11:46:09 -0300

    feat: exercicio24

---
+++ exercicios/exercicio24.js
@@ -0,0 +1,44 @@
+function makeMatrix() {
+  let matrix = [];
+  for (let i = 0; i < 6; i++) {
+    matrix[i] = [];
+    for (let j = 0; j < 8; j++) {
+      matrix[i][j] = Math.floor(Math.random() * 20) - 10; 
+    }
+  }
+  return matrix;
+}
+
+function negativeCounter(matrix) {
+  let vetorC = [];
+  for (let i = 0; i < matrix.length; i++) {
+    let count = 0;
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] < 0) {
+        count++;
+      }
+    }
+    vetorC.push(count);
+  }
+  return vetorC;
+}
+
+function showMatrix(matrix) {
+  for (let i = 0; i < matrix.length; i++) {
+    let row = "";
+    for (let j = 0; j < matrix[i].length; j++) {
+      row += matrix[i][j] + " ";
+    }
+    console.log(row);
+  }
+}
+
+let matrixM = makeMatrix();
+console.log("Matriz M:");
+showMatrix(matrixM);
+
+let vetorC = negativeCounter(matrixM);
+console.log("\nVetor C:");
+for (let i = 0; i < vetorC.length; i++) {
+  console.log(vetorC[i]);
+}
