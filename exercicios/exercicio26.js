986a0f90304f4d718c20cac7f31beb0f538ad6aa
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-05-10 14:33:31 -0300

    feat: exercicio 26

---
+++ exercicios/exercicio26.js
@@ -0,0 +1,33 @@
+function makeMatrix() {
+  let matrix = [];
+  for (let i = 0; i < 3; i++) {
+    matrix[i] = [];
+    for (let j = 0; j < 5; j++) {
+      matrix[i][j] = Math.floor(Math.random() * 10);
+    }
+  }
+
+  return matrix;
+}
+
+function multiplyMatrices(matrixA , matrixB){
+  let matrixP = [];
+  if (matrixA.length !== matrixB.length || matrixA[0].length !== matrixB[0].length) {
+    console.log('As matrizes precisam ter as mesmas dimensões para a multiplicação.');
+    return matrixP;
+  }
+  for (let i = 0; i < matrixA.length; i++) {
+    matrixP[i] = [];
+    for (let j = 0; j < matrixA[i].length; j++) {
+      matrixP[i][j] = matrixA[i][j] * matrixB[i][j];
+    }
+  }
+
+  return matrixP;
+
+}
+
+let matrixA = makeMatrix();
+let matrixB = makeMatrix();
+let productMatrix = multiplyMatrices(matrixA, matrixB);
+console.log(productMatrix);
