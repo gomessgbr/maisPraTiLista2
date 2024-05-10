6f92a35d4540f63b10bae674252d77c48c16649b
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-05-10 14:41:30 -0300

    feat: exercicio 27

---
+++ exercicios/exercicio27.js
@@ -0,0 +1,21 @@
+const prompt = require('prompt-sync')();
+
+function makeMatrix(){
+  let valueA = parseInt(prompt("Escreva um valor: "));
+  if(isNaN(valueA)){
+    return 'Valor inválido';
+  }
+  let matrix = [];
+  let result = [];
+  for(let i = 0; i < 6 ; i++){
+    matrix[i] = [];
+    for(let j = 0; j < 6; j++){
+      matrix[i][j] = Math.floor(Math.random() * 10);
+      result.push(matrix[i][j] * valueA);
+    }
+  }
+
+  return result;
+}
+
+console.log(makeMatrix());
