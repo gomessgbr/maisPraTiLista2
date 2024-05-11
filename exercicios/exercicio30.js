9662ae7e66ff03169837ddc134bfa8778ee0e87e
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-05-11 11:02:44 -0300

    feat: exercicio30

---
+++ exercicios/exercicio30.js
@@ -0,0 +1,30 @@
+function makeMatrix(){
+  let matrixM = [];
+  let vectorSL = [0,0,0,0,0];
+  let vectorSC = [0,0,0,0,0];
+  for(let i = 0; i < 5; i++){
+    matrixM[i] = [];
+    for(let j = 0; j < 5; j++){
+      matrixM[i][j] = Math.floor(Math.random() * 10);
+      vectorSC[j] += matrixM[i][j];
+      vectorSL[i]+= matrixM[i][j];
+    }
+  }
+
+  showMatrix(matrixM);
+  console.log('Vetor SL(5):', vectorSL);
+  console.log('Vetor SC(5):', vectorSC);
+}
+
+
+function showMatrix(matrix) {
+  for (let i = 0; i < matrix.length; i++) {
+    let row = "";
+    for (let j = 0; j < matrix[i].length; j++) {
+      row += matrix[i][j] + " ";
+    }
+    console.log(row);
+  }
+}
+
+makeMatrix();
